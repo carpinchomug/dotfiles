a55da39d5777eed551622b33de5096f9fce59b5c
Akiyoshi Suda <aki.suda@protonmail.com>
2022-03-28 22:25:37 +0900

    add blueman-applet service

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -3,4 +3,7 @@
 {
   services.gnome-keyring.enable = true;
   services.udiskie.enable = true;
+
+  # Note, for the applet to work, the 'blueman' service should be enabled system-wide.
+  services.blueman-applet.enable = true;
 }
