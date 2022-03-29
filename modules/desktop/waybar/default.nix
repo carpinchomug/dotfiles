116a074b5eda186b284c16d7f745c73bfc40c97b
Akiyoshi Suda <aki.suda@protonmail.com>
2022-03-29 18:59:56 +0900

    waybar init

--- modules/desktop/waybar/default.nix
+++ modules/desktop/waybar/default.nix
@@ -3,5 +3,18 @@
 {
   programs.waybar = {
     enable = true;
+
+    settings = {
+      layer = "top";
+      position = "top";
+
+      # modules-left = [ "sway/workspaces" "sway/window" ];
+      # modules-center = [ "clock" ];
+      # modules-right = [ "pulseaudio" "network" "cpu" "memory" "idle_inhibitor" "tray" ];
+
+      # style = ''
+      #   builtins.readFile ./style.css
+      # '';
+    };
   };
 }
