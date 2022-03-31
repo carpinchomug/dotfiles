91de3e5b1b0ecb71be9768071775038d63cd9f48
Akiyoshi Suda <aki.suda@protonmail.com>
2022-03-31 17:46:28 +0900

    change icons

--- modules/desktop/waybar/default.nix
+++ modules/desktop/waybar/default.nix
@@ -28,13 +28,13 @@
 
         pulseaudio = {
           format = "{volume}% {icon} {format_source}";
-          format-muted = "{icon}";
+          format-muted = " {format_source}";
           format-bluetooth = "{volume}% {icon} {format_source}";
-          format-bluetooth-muted = "{icon} {format_source}";
+          format-bluetooth-muted = " {format_source}";
           format-source = "{volume}% ";
           format-source-muted = "";
           format-icons = {
-            headphones = "";
+            headphone = "";
             default = "";
           };
           on-click = "pavucontrol";
