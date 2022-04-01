8bf17d4f949c59b754b4d787e1fa4ca9cbce83d8
Akiyoshi Suda <aki.suda@protonmail.com>
2022-04-01 23:28:10 +0900

    waybar

--- modules/desktop/waybar/default.nix
+++ modules/desktop/waybar/default.nix
@@ -81,6 +81,8 @@
           format-critical = "{capacity}% <span color='#eb6f92'>{icon}</span>";
           format-charging = "{capacity}% ";
           format-plugged = "{capacity}% ";
+          format-plagged-full = "{capacity}% ";
+
           format-icons = [ "" "" "" "" "" ];
         };
 
