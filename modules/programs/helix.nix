ff44baa4efe38cb03cf5759efd03683d434f593b
Akiyoshi Suda <aki.suda@protonmail.com>
2022-04-05 22:00:38 +0900

    helix

--- modules/programs/helix.nix
+++ modules/programs/helix.nix
@@ -6,6 +6,14 @@
 
     settings = {
       theme = "rose_pine"; 
+
+      # editor = {
+      #   cursor-shape = {
+      #     insert = "bar";
+      #     normal = "block";
+      #     select = "underline";
+      #   };
+      # };
     };
 
     # languages = [
