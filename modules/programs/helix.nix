fcbeb2a7c5cc327161e95852a45c3ade85edfb91
Akiyoshi Suda <aki.suda@protonmail.com>
2022-04-05 21:27:53 +0900

    helix: theme

--- modules/programs/helix.nix
+++ modules/programs/helix.nix
@@ -3,5 +3,17 @@
 {
   programs.helix = {
     enable = true;
+
+    settings = {
+      theme = "rose_pine"; 
+    };
+
+    # languages = [
+    #   {
+    #     name = "name of the language";
+    #     # options go here
+    #     # https://docs.helix-editor.com/languages.html
+    #   }
+    # ];
   };
 }
