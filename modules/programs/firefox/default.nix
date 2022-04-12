79c3b7b80e4235ae364553a3441e058f9ccdd0dd
Akiyoshi Suda <aki.suda@protonmail.com>
2022-04-12 16:43:02 +0900

    firefox settings

--- modules/programs/firefox/default.nix
+++ modules/programs/firefox/default.nix
@@ -4,10 +4,23 @@
   programs.firefox = {
     enable = true;
     package = pkgs.firefox-wayland;
+
+    profiles = {
+      default = {
+        isDefault = true;
+
+        userChrome = "${builtins.readFile ./chrome/userChrome.css}";
+        userContent = "${builtins.readFile ./chrome/userContent.css}";
+
+        settings = {
+          "browser.shell.checkDefaultBrowser" = false;
+        };
+      };
+    };
   };
 
   home.sessionVariables = {
     MOZ_ENABLE_WAYLAND = 1;
-    XDG_CURRENT_DESKTOP = "sway"; 
+    XDG_CURRENT_DESKTOP = "sway";
   };
 }
