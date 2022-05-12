ca84efe259ef26673ed1d693cc43f31ca3d52fb4
Akiyoshi Suda <aki.suda@protonmail.com>
2022-05-12 16:06:20 +0900

    fix nixos system update command

--- modules/shell/aliases.nix
+++ modules/shell/aliases.nix
@@ -2,7 +2,7 @@
 
 {
   home.shellAliases = {
-    nixos-switch = "sudo nixos-rebuild switch --flake ~/.config/nixos#thinkpad";
+    nixos-switch = "nixos-rebuild switch --use-remote-sudo --flake ~/.config/nixos#thinkpad";
     hm-switch = "home-manager switch --flake ~/.config/nixpkgs#akiyoshi";
     quickpy = "nix flake init --template github:carpinchomug/flake-templates#quickpython";
   };
