afadc0e75ef6daf79ac947a405a35663e166d97b
Akiyoshi Suda <aki.suda@protonmail.com>
2022-05-25 13:34:29 +0900

    helix: integrate texlab

--- modules/programs/helix/default.nix
+++ modules/programs/helix/default.nix
@@ -3,7 +3,7 @@
 
   imports = [
     ./config.nix
-    # ./languages.nix
+    ./languages.nix
     ./themes.nix
   ];
 }
--- modules/programs/helix/languages.nix
+++ modules/programs/helix/languages.nix
@@ -1,5 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  programs.helix.languages = { };
+  programs.helix.languages = [
+    {
+      name = "latex";
+      scope = "source.tex";
+      injection-regex = "tex";
+      file-types = [ "tex" "bib" ];
+      roots = [ ];
+      comment-token = "%";
+      language-server = { command = "texlab"; };
+      indent = { tab-width = 4; unit = "\t"; };
+    }
+  ];
 }
